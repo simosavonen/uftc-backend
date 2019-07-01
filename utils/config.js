ac7ec2f97ef365685b6db6be90339ff3988d1696
Simo Savonen <simo.savonen@gmail.com>
2019-07-01 09:08:03 +0300

    config works with locally installed MongoDB and without the .env file

--- utils/config.js
+++ utils/config.js
@@ -2,9 +2,9 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-let PORT = process.env.PORT;
-let MONGODB_URI = process.env.MONGODB_URI;
-const SECRET = process.env.SECRET;
+let PORT = process.env.PORT || 3001;
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/uftc";
+const SECRET = process.env.SECRET || "the_default_secret_string";
 
 if (process.env.NODE_ENV === "test") {
   PORT = process.env.TEST_PORT;
