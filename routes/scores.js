fe59e0a8fc18e274a734360a0bdcc9b41658c72c
Simo Savonen <simo.savonen@gmail.com>
2019-07-02 19:51:55 +0300

    scores päivittyy vain backendin toimesta, ei koskaan suoran API kutsun kautta kuten tässä meinasi tapahtua

--- routes/scores.js
+++ routes/scores.js
@@ -7,32 +7,4 @@ scoresRouter.get("/", async (req, res) => {
   res.json(scores.map(s => s.toJSON()));
 });
 
-// passport protected route(s)
-// http://www.passportjs.org/docs/authenticate/
-scoresRouter.post(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const scoreExists = await Score.findOne({
-      challenge: req.body.challenge,
-      user: req.body.user
-    });
-    if (scoreExists) {
-      scoreExists = {
-        ...scoreExists,
-        totalPoints: (scoreExists.totalPoints += req.body.points)
-      };
-
-      const updatedScore = await Score.findByIdAndUpdate(
-        scoreExists.id,
-        scoreExists,
-        { new: true }
-      );
-
-      res.json(updatedScore.toJSON());
-    } else {
-    }
-  }
-);
-
 module.exports = scoresRouter;
