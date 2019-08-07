ed566c9d439581e06ac3a0f566e52aff6e603a6a
Marko Mattila <mmattila75@gmail.com>
2019-08-07 11:38:36 +0300

    add workout on reset command

--- routes/testing.js
+++ routes/testing.js
@@ -1,11 +1,14 @@
 const testRouter = require("express").Router();
 const User = require("../models/User");
 const Challenge = require("../models/Challenge");
+const Activity = require("../models/Activity");
 const bcrypt = require("bcryptjs");
 
 testRouter.get("/reset", async (request, response) => {
-  await User.deleteMany({});
-  //make c onst challenge table
+  await User.deleteMany({}); //delete Users
+  await Challenge.deleteMany({}); //delete challenges
+  await Activity.deleteMany({}); //delete activities
+
   const password = "salasana";
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
@@ -18,30 +21,52 @@ testRouter.get("/reset", async (request, response) => {
     activeChallenge: null
   });
 
-  const testsavedUser = await testuser.save();
-  //console.log(testsavedUser);
-  //console.log("id", testsavedUser._id);
-  if (testsavedUser) {
-    const testchallenge = new Challenge({
-      name: "Syksy 19 megahaaste",
-      pointsGoal: 7500,
-      releaseDate: "2019-07-01",
-      startDate: "2019-08-01",
-      endDate: "2019-11-30",
-      deadline: "2019-12-14",
-      seriesTitle: "Kaikki pelaa!",
-      //description: req.body.description,
-      //icon: req.body.icon,
-      pointBonus: 1,
-      organizers: [testsavedUser._id]
-      //organizers: [testuser.user._id]
-      //activities: activityIDs
-    });
-    await testchallenge.save();
-  }
-  //const testcreatedChallenge = await testchallenge.save();
-  //    res.status(201).json(testcreatedChallenge.toJSON());
+  const testchallenge = new Challenge({
+    name: "Syksy 19 megahaaste",
+    pointsGoal: 7500,
+    releaseDate: "2019-07-01",
+    startDate: "2019-08-01",
+    endDate: "2019-11-30",
+    deadline: "2019-12-14",
+    seriesTitle: "Kaikki pelaa!",
+    //description: req.body.description,
+    //icon: req.body.icon,
+    pointBonus: 1,
+    //organizers: [testsavedUser._id]
+    organizers: [testuser._id]
+    //organizers: [testuser.user._id]
+    //activities: activityIDs
+  });
+  testuser.activeChallenge = testchallenge._id;
 
+  const activityTestFst = new Activity({
+    name: "Pushup - punnerus",
+    points: 4,
+    type: "Lihaskuntoharjoitus",
+    unit: "10 pushups",
+    description: "Punnerra jalat suorana",
+    url: "",
+    icon: "icon.svg"
+  });
+
+  const activityTestSnd = new Activity({
+    name: "Kahvakuulanosto",
+    points: 3,
+    type: "Lihaskuntoharjoitus",
+    unit: "5 nostoa",
+    description: "Nosta kahvakuulaa maan tasolta hartioiden yläpuolelle",
+    url: "http://fake.you.tube/123",
+    icon: "icon.svg"
+  });
+
+  const testsavedUser = await testuser.save();
+  console.log(testsavedUser);
+  const testChallenge = await testchallenge.save();
+  console.log(testChallenge);
+  const createdActivityFst = await activityTestFst.save();
+  const createdActivitySnd = await activityTestSnd.save();
+  console.log(createdActivityFst);
+  console.log(createdActivitySnd);
   response.status(204).end();
 });
 
